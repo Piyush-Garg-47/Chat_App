1145fcc4d9e8c03a4d02b8bb9cfb1b3b9a0ab1ac
Piyush Garg <piyushgarg809@gmail.com>
2025-03-04 23:43:04 +0530

    chat-place

--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className='chat-app'>
+        <form className='new-massage-form'> 
+            <input className='new-massage-input' placeholder='type your massage here...'/>
+            <button type='submit' className='send-button'>Send</button>
+        </form> 
+    </div>
   )
 }
 
