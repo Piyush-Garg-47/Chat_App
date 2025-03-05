309eb731d1399fd1239b9cd0ef21ae115823125f
Piyush Garg <piyushgarg809@gmail.com>
2025-03-05 22:53:38 +0530

    massaging workd processs

--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { useState } from 'react'
+import { addDoc , collection } from 'firebase/firestore'
+import { db } from '../firebase-config'
 const Chat = () => {
+    const [newMassage, setNewMassage] = useState("")
+    const messageRef = collection(db , 'massages') 
+    const handleSubmit = async (e) =>{ 
+        e.preventDefault() ; 
+      
+        if(newMassage === "") return  ; 
+         
+        await addDoc()
+    }
   return (
     <div className='chat-app'>
-        <form className='new-massage-form'> 
-            <input className='new-massage-input' placeholder='type your massage here...'/>
+        <form onSubmit={handleSubmit} className='new-massage-form'> 
+            <input onChange={(e) =>setNewMassage(e.target.value)} className='new-massage-input' placeholder='type your massage here...'/>
             <button type='submit' className='send-button'>Send</button>
         </form> 
     </div>
