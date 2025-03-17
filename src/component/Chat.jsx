18a6ac54e8b1353aefdc1bf8fe12e0612afead13
Piyush Garg <piyushgarg809@gmail.com>
2025-03-17 17:08:42 +0530

    chat progress

--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { useState } from 'react'
-import { addDoc , collection ,serverTimestamp } from 'firebase/firestore'
+import { addDoc , collection ,serverTimestamp , onSnapshot, query, where } from 'firebase/firestore'
 import { db ,auth } from '../firebase-config'
 import '../styles/Style.css'; 
 
 const Chat = (props) => {
     const {room} = props ; 
     const [newMassage, setNewMassage] = useState("")
+    const [message, setmessage] = useState([]) 
     const messageRef = collection(db , 'massages') 
+     
+    useState(() =>{
+        const queryMasseges = query(messageRef , where("room", "==" ,room)) ; 
+          const unsuscribe = onSnapshot(queryMasseges , (snapShot)=>{
+           let messages = [] ; 
+               snapShot.forEach((doc) =>{
+                    messages.push({...doc.data() , id: doc.id})
+               })
+          setmessage(messages) ; 
+         }) 
+         return ()=> unsuscribe() ; 
+    },[])
+
     const handleSubmit = async (e) =>{ 
         e.preventDefault() ; 
       
@@ -19,9 +33,11 @@ const Chat = (props) => {
             user : auth.currentUser.displayName ,
              room , 
         })
+        setNewMassage("") ; 
     }
   return (
     <div className='chat-app'>
+        <div>{message.map((message) => <h1>{message.text} </h1> )}</div>
         <form onSubmit={handleSubmit} className='new-massage-form'> 
             <input onChange={(e) =>setNewMassage(e.target.value)} className='new-massage-input' placeholder='type your massage here...'/>
             <button type='submit' className='send-button'>Send</button>
